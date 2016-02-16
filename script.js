f6aa718d5cd2330e7406cfaa03c08fb1148b3ca0
Cody13X <eyannick13@gmail.com>
2016-02-16 11:30:13 +0100

    quelques routes

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  alert('piouf');
+  $("#register").on("submit", function(e) {
+    //e.preventDefault();
+    alert('piouf');
+    //requequette serveur pour l'inscription
+    $.ajax({
+       url : 'register.php' // La ressource ciblée
+    });
+  });
+  $("#login").on("submit", function(e) {
+    //e.preventDefault();
+    //requequette serveur pour le log
+    $.ajax({
+       url : 'login.php' // La ressource ciblée
+    });
+  });
+  $("#logoff").on("submit", function(e) {
+    //e.preventDefault();
+    //requequette serveur pour le délog
+    $.ajax({
+       url : 'logoff.php' // La ressource ciblée
+    });
+  });
 })
