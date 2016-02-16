68c384d9af0b1641a0cca5815c549f8d861b3d44
Cody13X <eyannick13@gmail.com>
2016-02-16 16:33:32 +0100

    requete formulaire + server local

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
   $("#register").on("submit", function(e) {
-    //e.preventDefault();
-    alert('piouf');
+    e.preventDefault();
+    
     //requequette serveur pour l'inscription
-    $.ajax({
-       url : 'register.php' // La ressource ciblée
-    });
+    var $this = $(this); // L'objet jQuery du formulaire
+    // Je récupère les valeurs
+    var pseudo = $('#prenom').val();
+    var mail = $('#emailRegisterConfirme').val();
+
+    // Je vérifie une première fois pour ne pas lancer la requête HTTP
+    // si je sais que mon PHP renverra une erreur
+    if(pseudo === '' || mail === '') {
+      alert('Les champs doivent êtres remplis');
+    } else {
+    // Envoi de la requête HTTP en mode asynchrone
+      $.ajax({
+        url: 'http://localhost/bonobo-server/register.php', // Le nom du fichier indiqué dans le formulaire
+        type: "POST", // La méthode indiquée dans le formulaire (get ou post)
+        data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          alert(response); // J'affiche cette réponse
+        }
+      });
+    }
   });
   $("#login").on("submit", function(e) {
     //e.preventDefault();
