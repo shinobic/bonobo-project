7fb09085299e95712bb331dff9498675fa310600
Cody13X <eyannick13@gmail.com>
2016-02-25 14:07:42 +0100

    modification suppression tweet

--- script.js
+++ script.js
@@ -6,27 +6,33 @@ $(document).ready(function() {
     var $this = $(this); // L'objet jQuery du formulaire
     // Je récupère les valeurs
     var pseudo = $('#prenom').val();
-    var mail = $('#emailRegisterConfirme').val();
+    var mail = $('#emailRegister').val();
+    var cmail = $('#emailRegisterConfirme').val();
+    var password = $('#password').val();
 
     // Je vérifie une première fois pour ne pas lancer la requête HTTP
     // si je sais que mon PHP renverra une erreur
-    if(pseudo === '' || mail === '') {
-      alert('Les champs doivent êtres remplis');
-    } else {
-    // Envoi de la requête HTTP en mode asynchrone
+    if(pseudo === '' || mail === '' || cmail === '' || password === '') {
+      alert("Les champs doivent êtres remplis");
+    } else if(mail !== cmail) {
+      alert("Vérifier les adresses mail !");
+      alert(mail + cmail);
+    }
+    else {
+      // Envoi de la requête HTTP en mode asynchrone
       $.ajax({
         url: 'http://localhost/bonobo-server/subscribe.php', // Le nom du fichier indiqué dans le formulaire
         type: "POST", // La méthode indiquée dans le formulaire (get ou post)
         data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
         success: function(response) { // Je récupère la réponse du fichier PHP
-          alert(response); // J'affiche cette réponse
+          //alert(response); // J'affiche cette réponse
           //redirection vers la page des tweets
           var href = 'tweets.html';
           $(location).attr('href','tweets.html?pseudo=' + pseudo);
         },
         error : function(resultat, statut, erreur){
           //traite l'erreur
-          alert(resultat);
+          alert(statut + " " + erreur);
         }
       });
     }
--- script2.js
+++ script2.js
@@ -1,38 +1,27 @@
 $(document).ready(function() {
   var sVar = "pseudo";
+  var tids = [];
   sVar = window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(sVar).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1");
-  //pas d'acces direct si no parameters
-  if(sVar === ''){
+  //pas d'acces direct si no parameters ou mauvais user
+  if(sVar !== "Sdz") {
+    //page 404 !
     var href = 'index.html';
     $(location).attr('href', 'index.html');
   }
-  //requete de connexion pour être sûr que le user existe
-  $.ajax({
-/*    url: 'http://localhost/bonobo-server/login.php', // Le nom du fichier indiqué dans le formulaire
-    type: "POST", // La méthode indiquée dans le formulaire (get ou post)
-    data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
-    success: function(response) { // Je récupère la réponse du fichier PHP
-      alert(response); // J'affiche cette réponse
 
-      var href = 'tweets.html';
-      $(location).attr('href','tweets.html?pseudo=' + pseudo);
-    },
-    error : function(resultat, statut, erreur){
-      //traite l'erreur
+  //message de bienvenue
+  $('.box').append('<p style="color: white;">' + "Bienvenue au bonobo : "  + sVar + '</p>');
 
-    }*/
-  });
-
-  var texte = $("#loginame").text();
-  $("#loginame").text(texte + " " + sVar);
   //requete serveur pour recevoirs les tweets
   $.ajax({
     dataType: "json",
     url : 'http://localhost/bonobo-server/tweets.php?action=query', // La ressource ciblée
     success: function(json) { // Je récupère la réponse du fichier PHP
       //alert(json); // J'affiche cette réponse
-      for(var i=0;i<json.length;i++){
-            $('.thetweets').append('<p>' + json[i].pseudo + " " + json[i].age + " " + json[i].tweet + '</p>');
+      for(var i=0; i<json.length; i++){
+        $('.effect').append('<div class="tweet">' + '<p style="display: inline">' + "Le " + json[i].date + " à " + json[i].time + " " + json[i].pseudo + " à posté : " + json[i].tweet + '</p>' +
+         " " + '<i style="color: red" class="fa fa-heart-o"></i>' + '<span> </span>' + '<i class="fa fa-clone"></i>' + '<p></p>' + '</div>');
+         //tids[i].push(json[i].tid);
       }
     },
     error : function(resultat, statut, erreur){
@@ -43,98 +32,126 @@ $(document).ready(function() {
 
   //boucle events
   function onTick() {
-  $('#postweet').off('keypress').on("keypress", function(e) {
-    if(e.which == 13) {
-      var val = $(this).val();
+    //Poste un tweet
+    $('#send').off('click').on("click", function() {
+      var val = $("textarea").val();
+
       //requete serveur pour poster le tweet
-      var $this = $(this); // L'objet jQuery de l'input
-      this.value = '';
+      var $this = $("textarea"); // L'objet jQuery de l'input
+
+      $("textarea").val('');
       $.ajax({
-      url : 'http://localhost/bonobo-server/postweet.php', // La ressource ciblée
-      type: "POST", // La méthode indiquée dans le formulaire (get ou post)
-      data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans l'input)
-      success: function(response) { // Je récupère la réponse du fichier PHP
-        //alert(response); // J'affiche cette réponse
+        url : 'http://localhost/bonobo-server/postweet.php?action=post', // La ressource ciblée
+        type: "POST", // La méthode indiquée dans le formulaire (get ou post)
+        data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans l'input)
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          //alert(response); // J'affiche cette réponse
           //good on peut afficher le tweet
-          $('.thetweets').prepend('<div class="pipo">' + '<p class="json">' + sVar + " " + "41" + " " + val + '</p> ' + '<i class="fa fa-times">' + '</div>');
+          var dt = new Date();
+          var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+          $('.effect').prepend('<div class="bonobo">' + '<p style="display: inline; margin-bottom: 1vh" class="json">' + "Le " + dt.getDate() + "/"  + dt.getFullYear()
+           + " à " + time + " " + sVar + " " + "à posté : " + val + '</p> ' + '<i style="color: red" class="fa fa-times"></i>' + '<span> </span>' +
+           '<i style="color: red" class="fa fa-heart-o"></i>' + '<span> </span>' + '<i class="fa fa-clone"></i>' + '<p></p>' + '</div>');
         },
         error : function(resultat, statut, erreur){
           //traite l'erreur
           alert(erreur);
         }
       });
-    }
-  });
-
-  $(".logoff").on("click", function(e) {
-    //requete serveur pour le délog
-    $.ajax({
-      url : 'http://localhost/bonobo-server/logoff.php?action=logoff', // La ressource ciblée
-      success: function(response) { // Je récupère la réponse du fichier PHP
-        alert(response); // J'affiche cette réponse
-        var href = 'index.html';
-        $(location).attr('href', 'index.html');
-      },
-      error : function(resultat, statut, erreur){
-         //traite l'erreur
-      //   alert(erreur);
-      }
     });
-  });
 
-  $(".unregister").on("click", function(e) {
-    //requete serveur pour la désinscription
-    $.ajax({
-      url : 'http://localhost/bonobo-server/unregister.php?action=unregister', // La ressource ciblée
-      success: function(response) { // Je récupère la réponse du fichier PHP
-        alert(response); // J'affiche cette réponse
-        var href = 'index.html';
-        $(location).attr('href', 'index.html');
-      },
-      error : function(resultat, statut, erreur){
-        //traite l'erreur
-        alert(erreur);
-      }
-    });
-  });
-
-  //remove
-  $('.fa-times').off('click').on("click", function() {
-    alert('pioyf');
-    //requete serveur pour supprimer son tweet
-    $.ajax({
-      url : 'http://localhost/bonobo-server/tweetoff.php?action=delete', // La ressource ciblée
-      success: function(response) { // Je récupère la réponse du fichier PHP
-        alert(response); // J'affiche cette réponse
-      },
+    //
+    $("#logoff").off('click').on("click", function() {
+      //requete serveur pour le délog
+      $.ajax({
+        url : 'http://localhost/bonobo-server/logoff.php?action=logoff', // La ressource ciblée
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          //alert(response); // J'affiche cette réponse
+          var href = 'index.html';
+          $(location).attr('href', 'index.html');
+        },
         error : function(resultat, statut, erreur){
-        //traite l'erreur
-        //alert(erreur);
-      }
+         //traite l'erreur
+         alert(erreur);
+        }
+      });
     });
-  });
 
-  /*$("#tweetoff").on("submit", function(e) {
-    //e.preventDefault();
-    //requete serveur pour supprimer son tweet
-    $.ajax({
-       url : 'tweetoff.php' // La ressource ciblée
+    $("#unsubscribe").off('click').on("click", function() {
+      //requete serveur pour la désinscription
+      $.ajax({
+        url : 'http://localhost/bonobo-server/unsubscribe.php?action=unsubscribe', // La ressource ciblée
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          //alert(response); // J'affiche cette réponse
+          var href = 'index.html';
+          $(location).attr('href', 'index.html');
+        },
+        error : function(resultat, statut, erreur){
+          //traite l'erreur
+          alert(erreur);
+        }
+      });
     });
-  });*/
-  $("#like").on("submit", function(e) {
-    //e.preventDefault();
-    //requete serveur pour le like
-    $.ajax({
-       url : 'like.php' // La ressource ciblée
+
+    //select un tweet
+/*    $('.tweet').off('click').on("click", function() {
+      $(this).addClass('Tclicked');
+    });*/
+
+    //remove
+    $('.fa-times').off('click').on("click", function() {
+      var mydiv = $(this).parent(".bonobo");
+      //requete serveur pour supprimer son tweet
+      $.ajax({
+        url : 'http://localhost/bonobo-server/tweetoff.php?action=delete', // La ressource ciblée
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          //remove le tweet selectionné
+          mydiv.remove();
+        },
+        error : function(resultat, statut, erreur){
+          //traite l'erreur
+          alert(erreur);
+        }
+      });
     });
-  });
-  $("#retweet").on("submit", function(e) {
-    //e.preventDefault();
-    //requete serveur pour le délog
-    $.ajax({
-       url : 'retweet.php' // La ressource ciblée
+
+    $('.fa-heart-o').off('click').on("click", function() {
+      var mydiv = $(this).parent(".tweet");
+      //requete serveur pour liker un tweet
+      $.ajax({
+        //on indique au serveur qu'on a liké avec envois de l'id
+        url : 'http://localhost/bonobo-server/tweetlike.php', // La ressource ciblée
+        type: "POST", // La méthode indiquée dans le formulaire (get ou post)
+        /*data: $this.serialize(),*/ // Je sérialise les données (j'envoie toutes les valeurs présentes dans l'input)
+        success: function(response) { // Je récupère la réponse du fichier PHP
+          //alert(response);
+          //mark as liked
+          mydiv.addClass('liked');
+        },
+        error : function(resultat, statut, erreur){
+          //traite l'erreur
+          alert(resultat.status);
+        }
+      });
     });
-  });
-}
+
+    $(".fa-clone").off('click').on("click", function() {
+      var val = $(".Tclicked p:first").text();
+      alert(val);
+      //requete serveur pour retweeter
+      $.ajax({
+         url : 'http://localhost/bonobo-server/retweet.php', // La ressource ciblée
+         success: function(response) { // Je récupère la réponse du fichier PHP
+           //mark as retweeted
+           //alert(response)
+          // $(".Tclicked").addClass('retweeted').removeClass("Tclicked");
+         },
+         error : function(resultat, statut, erreur){
+           //traite l'erreur
+           alert(statut);
+         }
+      });
+    });
+  }
   setInterval(onTick, 500);
 })
